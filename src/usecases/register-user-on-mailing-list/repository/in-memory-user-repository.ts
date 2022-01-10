bfdf67a9c8335f277eddf416db9599e186d1b97d
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-10 10:18:19 -0300

    refactor: use find instead of filter while finding user by email

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -16,17 +16,8 @@ export class InMemoryUserRepository implements UserRepository {
     }
 
     async findUserByEmail (email: string): Promise<UserData> {
-      // const users = this.repository.filter(user => {
-      //   return user.email === email
-      // })
-      // if (users.length > 0) {
-      //   return users[0]
-      // }
-      // return null
-
-      let user = this.repository.find(user => user.email === email)
-      if (!user) user = null
-      return user
+      const found = this.repository.find(user => user.email === email)
+      return found || null
     }
 
     async findAllUsers (): Promise<UserData[]> {
